ff37ad5c5b065e24ebeeb03db072a5d33d755003
ps4449 <priyasrini55@gmail.com>
2021-09-27 23:31:40 +0530

    Searching the deal

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ with Booking() as bot:
     bot.first_page()
     bot.change_currency(currency='USD')
     bot.select_place("New York")
-    bot.select_dates(checkin='2021-09-22', checkout='2021-09-25')
+    bot.select_dates(checkin='2021-09-30', checkout='2021-10-10')
+    bot.select_members(10)
     print("Exiting...")
--- venv/Bot/booking.py
+++ venv/Bot/booking.py
@@ -37,15 +37,43 @@ class Booking(webdriver.Chrome):
         first_result = self.find_elements_by_css_selector(
             'li[data-i="0"]'
         )
-        first_result.click()
+        first_result[0].click()
 
     def select_dates(self, checkin, checkout):
         checkin = self.find_elements_by_css_selector(
            f'td[data-date="{checkin}"]'
         )
-        checkin.click()
+        checkin[0].click()
 
         checkout = self.find_elements_by_css_selector(
             f'td[data-date="{checkout}"]'
         )
-        checkout.click()
+        checkout[0].click()
+
+    def select_members(self, count):
+        selection_element = self.find_element_by_id('xp__guests__toggle')
+        selection_element.click()
+
+        while True:
+            decrease_adults = self.find_elements_by_css_selector(
+                'button[aria-label="Decrease number of Adults"]'
+            )
+            decrease_adults[0].click()
+
+            adults_value_ele = self.find_element_by_id('group_adults')
+            adults_value = adults_value_ele.get_attribute('value')
+
+            if int(adults_value) == 1:
+                break
+
+        increase_adults = self.find_elements_by_css_selector(
+            'button[aria-label="Increase number of Adults"]'
+        )
+        for i in range(1, count):
+            increase_adults[0].click()
+
+    def click_search(self):
+        search_button = self.find_elements_by_css_selector(
+            'button[type="submit"]'
+        )
+        search_button.click()
